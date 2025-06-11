ee1fa1b7a42b6d6eba6101e13165b3231ed2286f
G061206 <111353915+G061206@users.noreply.github.com>
2025-06-12 01:40:04 +0800

    Delete src directory

--- src/lib/kv.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@vercel/kv';
-
-// 使用Vercel KV客户端
-export const kv = createClient({
-  url: process.env.KV_URL || "https://optimal-urchin-32101.upstash.io",
-  token: process.env.KV_REST_API_TOKEN || "AX1lAAIjcDFiYjgyZGNmNjI5OTc0YTJiODg1MmQ5NmY2YzEwYzM0YnAxMA"
-}); 
