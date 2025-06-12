4de4610cb67f637ab9217128aa808182188e2e6e
G061206 <111353915+G061206@users.noreply.github.com>
2025-06-13 00:21:09 +0800

    Update kv.ts

--- src/lib/kv.ts
+++ src/lib/kv.ts
@@ -2,6 +2,6 @@ import { createClient } from '@vercel/kv';
 
 // 使用Vercel KV客户端
 export const kv = createClient({
-  url: process.env.KV_REST_API_URL || "https://optimal-urchin-32101.upstash.io",
-  token: process.env.KV_REST_API_TOKEN || "AX1lAAIjcDFiYjgyZGNmNjI5OTc0YTJiODg1MmQ5NmY2YzEwYzM0YnAxMA"
+  url: process.env.KV_REST_API_URL,
+  token: process.env.KV_REST_API_TOKEN
 }); 
