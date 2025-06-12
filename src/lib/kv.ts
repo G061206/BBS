109310e0e4c0d0bbb7aafa23ab76624de17476b9
G061206 <111353915+G061206@users.noreply.github.com>
2025-06-12 11:46:42 +0800

    Delete src directory

--- src/lib/kv.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@vercel/kv';
-
-// 使用Vercel KV客户端
-export const kv = createClient({
-  url: "https://optimal-urchin-32101.upstash.io",
-  token: "AX1lAAIjcDFiYjgyZGNmNjI5OTc0YTJiODg1MmQ5NmY2YzEwYzM0YnAxMA"
-}); 
